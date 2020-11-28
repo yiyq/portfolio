591dd7858f2c31101d0dcb15ded8066b5906e39f
Yunqing Yi <yiyq43@gmail.com>
2020-11-27 22:39:26 -0500

    Remove footer css

--- src/components/streamLabs/StreamLabs.jsx
+++ src/components/streamLabs/StreamLabs.jsx
@@ -92,7 +92,6 @@ class StreamLabs extends React.Component {
                 <div className="streamlabs-prototype-container">
                     <h1>Prototype</h1>
                 </div>
-                <div className="footer"/>
             </div>
         );
     }
