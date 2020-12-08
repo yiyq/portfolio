acca2130907fc4bb2ae3bcc750e005bba65e709e
Yunqing Yi <yiyq43@gmail.com>
2020-12-07 21:52:12 -0500

    Fix 404

--- src/components/Footer/NavFooter.jsx
+++ src/components/Footer/NavFooter.jsx
@@ -4,13 +4,31 @@ import { withRouter } from 'react-router';
 import './NavFooter.scss'
 
 class NavFooter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick(event) {
+        this.props.history.push(`/portfolio/${event}`);
+    }
+
     render() {
         return (
             <div className="nav-footer">
-                <div className="nav-footer-item"><a href="/portfolio">All</a></div>
-                <div className="nav-footer-item"><a href="/portfolio/streamLabs">Streamlabs</a></div>
-                <div className="nav-footer-item"><a href="/portfolio/medforall">Medforall</a></div>
-                <div className="nav-footer-item"><a href="/portfolio/ohioathome">Ohioathome</a></div>
+                <div className="nav-footer-item" onClick={() => this.handleOnClick('')}>
+                    All
+                </div>
+                <div className="nav-footer-item" onClick={() => this.handleOnClick('streamLabs')} >
+                    Streamlabs
+                </div>
+                <div className="nav-footer-item" onClick={() => this.handleOnClick('medforall')} >
+                    Medforall
+                </div>
+                <div className="nav-footer-item" onClick={() => this.handleOnClick('ohioathome')} >
+                    Ohioathome
+                </div>
             </div>
         );
     }
